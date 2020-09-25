7b9f8f4bcc7182d1d05d5c627297928e64ef592c
Felipe Igawa Moskado <felipe.moskado@objective.com.br>
2020-09-25 10:50:07 -0300

    Verifica se arquivo existe

--- check-version.sh
+++ check-version.sh
@@ -3,8 +3,13 @@
 BASE_DIR="/home/felipe/Downloads/teste"
 CHANGE_FILE="$BASE_DIR/patch-10.14b84/changed_files.txt"
 
+if [ ! -e $CHANGE_FILE ]
+then
+    echo "Arquivo inexistente."
+fi
+
 #Lê o arquivo
-while read line; do 
+while read line; do
     echo line
 
 done < $CHANGE_FILE
